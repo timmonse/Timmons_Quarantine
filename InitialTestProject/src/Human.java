763a27289e8a151330840f8f425792b25440a11a
Evan Timmons <evangeorgetimmons@gmail.com>
2020-04-04 23:45:11 -0500

    Added human class and initial print statement

---
+++ InitialTestProject/src/Human.java
@@ -0,0 +1,21 @@
+public class Human {
+    private int age;
+    private String name;
+
+    public Human(){
+
+    }
+
+    public Human(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
--- InitialTestProject/src/LifeGameEvan.java
+++ InitialTestProject/src/LifeGameEvan.java
@@ -1,11 +1,45 @@
-public class LifeGameEvan {
-    private int age = 0;
+import java.util.Scanner;
 
-    public static void main(String args[]){
+public class LifeGameEvan {
+    private static int playerAge = 0;
+    private static String playerName = "";
+
+    public static void main(String args[]) {
         startGame();
     }
 
-    public static void startGame(){
+    private static void startGame() {
+        displayStartArt();
+        Scanner scan = new Scanner(System.in);
+        Human mainCharacter = new Human(playerAge, playerName);
+
+    }
+
+    private static void displayStartArt() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(" /$$      /$$           /$$                                                     /$$                    \r\n");
+        sb.append("| $$  /$ | $$          | $$                                                    | $$                    \r\n");
+        sb.append("| $$ /$$$| $$  /$$$$$$ | $$  /$$$$$$$  /$$$$$$  /$$$$$$/$$$$   /$$$$$$        /$$$$$$    /$$$$$$       \r\n");
+        sb.append("| $$/$$ $$ $$ /$$__  $$| $$ /$$_____/ /$$__  $$| $$_  $$_  $$ /$$__  $$      |_  $$_/   /$$__  $$      \r\n");
+        sb.append("| $$$$_  $$$$| $$$$$$$$| $$| $$      | $$  \\ $$| $$ \\ $$ \\ $$| $$$$$$$$        | $$    | $$  \\ $$      \r\n");
+        sb.append("| $$$/ \\  $$$| $$_____/| $$| $$      | $$  | $$| $$ | $$ | $$| $$_____/        | $$ /$$| $$  | $$      \r\n");
+        sb.append("| $$/   \\  $$|  $$$$$$$| $$|  $$$$$$$|  $$$$$$/| $$ | $$ | $$|  $$$$$$$        |  $$$$/|  $$$$$$/      \r\n");
+        sb.append("|__/     \\__/ \\_______/|__/ \\_______/ \\______/ |__/ |__/ |__/ \\_______/         \\___/   \\______/       \r\n");
+        sb.append("                                                                                                       \r\n");
+        sb.append("                                                                                                       \r\n");
+        sb.append("                                                                                                       \r\n");
+        sb.append(" /$$$$$$$$ /$$$$$$$$ /$$   /$$ /$$$$$$$$        /$$$$$$        /$$       /$$$$$$ /$$$$$$$$ /$$$$$$$$   \r\n");
+        sb.append("|__  $$__/| $$_____/| $$  / $$|__  $$__/       /$$__  $$      | $$      |_  $$_/| $$_____/| $$_____/   \r\n");
+        sb.append("   | $$   | $$      |  $$/ $$/   | $$         |__/  \\ $$      | $$        | $$  | $$      | $$         \r\n");
+        sb.append("   | $$   | $$$$$    \\  $$$$/    | $$           /$$$$$$/      | $$        | $$  | $$$$$   | $$$$$      \r\n");
+        sb.append("   | $$   | $$__/     >$$  $$    | $$          /$$____/       | $$        | $$  | $$__/   | $$__/      \r\n");
+        sb.append("   | $$   | $$       /$$/\\  $$   | $$         | $$            | $$        | $$  | $$      | $$         \r\n");
+        sb.append("   | $$   | $$$$$$$$| $$  \\ $$   | $$         | $$$$$$$$      | $$$$$$$$ /$$$$$$| $$      | $$$$$$$$   \r\n");
+        sb.append("   |__/   |________/|__/  |__/   |__/         |________/      |________/|______/|__/      |________/ \r\n");
+
+
+        System.out.println(sb);
 
     }
 
