833495a9cded6181f2866311a0bb781859de0011
Evan Timmons <evangeorgetimmons@gmail.com>
2020-04-04 22:22:30 -0500

    Added startGame function

--- InitialTestProject/src/Text2Life_Main.java
+++ InitialTestProject/src/Text2Life_Main.java
@@ -2,9 +2,13 @@ public class Text2Life_Main {
 
     public static void main(String args[]){
 
-
+        startGame();
         int age = 0;
         System.out.println(age);
+
+    }
+
+    public static void startGame(){
 
     }
 }
