7ae7910ac1bff8221626a4b54ca1427f83ae129c
Niko Timmons <nikotimmons9@gmail.com>
2020-04-05 00:04:06 -0500

    added basics of death method

--- InitialTestProject/src/Text2Life_Main.java
+++ InitialTestProject/src/Text2Life_Main.java
@@ -1,15 +1,64 @@
-public class Text2Life_Main {
+import java.util.Random;
+import java.util.Scanner;
 
-    public static void main(String args[]){
+public class Text2Life_Main {
+    private static int age = 0;
+    private static Random random = new Random();
+    private static boolean Alive = true;
+    private static int deathnumber = 0;
+
+    public static void main(String args[]) {
 
         startGame();
-        int age = 0;
-        //System.out.println(age);
-
-
+        mainGame();
     }
 
-    public static void startGame(){
+    private static void startGame() {
+        System.out.println("age " + age + ": You have been born");
+    }
 
+    private static void mainGame() {
+        while (Alive == true) {
+            everyYear();
+            deathChances();
+        }
+    }
+
+    private static void everyYear() {
+        String test = "dude";
+        Scanner checkEnter = new Scanner(System.in);
+        test = checkEnter.nextLine();
+        if (test.equals("")) {
+            age++;
+            System.out.println("you are " + age + " years old");
+            deathnumber = random.nextInt(10);
+        }
+    }
+
+    private static void deathChances() {
+
+        if(deathnumber == 1) {
+            Alive = false;
+            System.out.println(" ☠ you are dead of cancer ☠ ");
+        }
+        else if(deathnumber == 2) {
+            Alive = false;
+            System.out.println(" ☠ you are dead of covid-19 ☠ ");
+        }
+        else if(deathnumber == 3) {
+            Alive = false;
+            System.out.println(" ☠ you are dead of old age ☠ ");
+        }
+        else if(deathnumber == 4) {
+            Alive = false;
+            System.out.println(" ☠ you are dead of an overdose ☠ ");
+        }
+        else if(deathnumber == 5) {
+            Alive = false;
+            System.out.println(" ☠ you are dead due to falling off a cliff ☠ ");
+        }
+        else {
+            Alive = true;
+        }
     }
 }
