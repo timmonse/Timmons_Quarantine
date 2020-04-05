0fd3c635bcd7b1f013c682d04588a1fbd3a32e4c
Evan Timmons <evangeorgetimmons@gmail.com>
2020-04-04 22:21:14 -0500

    added lifeGameEvan

---
+++ InitialTestProject/src/LifeGameEvan.java
@@ -0,0 +1,12 @@
+public class LifeGameEvan {
+    private int age = 0;
+
+    public static void main(String args[]){
+        startGame();
+    }
+
+    public static void startGame(){
+
+    }
+
+}
